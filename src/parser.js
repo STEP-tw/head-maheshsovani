ebc69e2ef523ff69c963f0833a9ae6b56ea0bc73
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-12-13 16:49:41 +0530

    Modified parse input function in parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,27 +1,27 @@
 const parseInput = function (details) {
-  let organisedData = { option: "n", count: 10, files: details.slice(0) };
+  let defaultParameters = { option: "n", count: 10, files: details };
   if (details[0] == "-n" || details[0] == "-c") {
-    organisedData = {
+    return {
       option: details[0][1],
       count: details[1],
       files: details.slice(2)
     };
   }
   if (details[0][0] == "-" && details[0].length > 2) {
-    organisedData = {
+    return {
       option: details[0][1],
       count: details[0].slice(2),
       files: details.slice(1)
     };
   }
   if (parseInt(details[0])) {
-    organisedData = {
+    return {
       option: "n",
       count: Math.abs(details[0]),
       files: details.slice(1)
     };
   }
-  return organisedData;
+  return defaultParameters;
 };
 
 module.exports = { parseInput }
